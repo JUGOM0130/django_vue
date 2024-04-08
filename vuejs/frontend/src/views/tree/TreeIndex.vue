ed7a249152c42a9ecd3ea2e7001015325c27d5fa
JUGOM0130 <junnehoomaki@yahoo.co.jp>
2024-04-08 22:09:52 +0900

    疲れたやめる

--- vuejs/frontend/src/views/tree/TreeIndex.vue
+++ vuejs/frontend/src/views/tree/TreeIndex.vue
@@ -1,22 +1,57 @@
 <script setup>
-import { ref } from 'vue'
-let node = ref(0);
-let countup = () =>{
-    node.value++;
+import { defineProps, ref } from 'vue'
+import ThisComponet from './TreeIndex.vue'
+
+const props = defineProps({
+    parentobj: Object
+})
+/**デフォルトオブジェクト */
+let o = ref([
+    {
+        id: "1",
+        child: [
+            {
+                id: "2",
+                child: [
+
+                ]
+            }
+        ],
+    }, {
+        id: "3",
+        child: [
+            {
+                id: "4",
+                child: [
+
+                ]
+            }
+        ]
+    }
+])
+
+if (props.parentobj != null) {
+    /*オブジェクト上書き */
+    o = ref(props.value);
 }
+
+console.log(o.value)
+
+
 </script>
 <template>
     <div>
         <ul>
-            <li @click="countup">
-                ALC-AA000Z000{{ node }}
+            <li v-for="d in o" :key="d.id">
+                {{ d.id }}
+                <ThisComponet :parentobj="d.child" />
             </li>
         </ul>
     </div>
 </template>
 <style>
 * {
-    margin:0;
-    padding:0;
+    margin: 0;
+    padding: 0;
 }
 </style>
