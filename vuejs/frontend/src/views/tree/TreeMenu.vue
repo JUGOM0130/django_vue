0e420838620124fdffa4730bc9a35e6b1e4e0892
JUGOM0130 <junnehoomaki@yahoo.co.jp>
2024-10-09 22:27:23 +0900

    vuetify修正

--- vuejs/frontend/src/views/tree/TreeMenu.vue
+++ vuejs/frontend/src/views/tree/TreeMenu.vue
@@ -1,39 +1,67 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-const port_message_visible = !(process.env.NODE_ENV === 'production');
+const port_message_visible = !(process.env.NODE_ENV === "production");
 const router = useRouter();
 
 const homeClick = () => {
-  router.push('/')
-}
-const treeClick = () => {
-  router.push('/test')
-}
-const treeComponentClick = () => {
-  router.push('/treecomponent')
-}
+  router.push("/");
+};
+
 const treeComponent2Click = () => {
-  router.push('/treecomponent2')
-}
+  router.push("/treecomponent2");
+};
+const dismissBanner = ref(true);
 </script>
 <template>
   <div>
     <v-app>
       <v-main>
-        <h1>TreeIndex(Menu)</h1>
-        <h4 v-if="port_message_visible">※検証環境DjangoのPortは8001です</h4>
-        <a href="http://133.18.242.137/api/test" blank="_self">133.18.242.137</a>
-        <div>
-          <v-btn variant="outlined" @click="homeClick">HOME</v-btn>
-          <v-btn variant="outlined" @click="treeClick">Tree</v-btn>
-          <v-btn variant="outlined" @click="treeComponentClick">TreeComponent</v-btn>
-          <v-btn variant="outlined" @click="treeComponent2Click">TreeComponent2</v-btn>
-        </div>
-        <router-view />
+        <!--Application Bar-->
+        <v-app-bar :elevation="2">
+          <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          </template>
+
+          <v-row class="align-center">
+            <v-col cols="auto">
+              <v-app-bar-title>TreeMenu.vue</v-app-bar-title>
+            </v-col>
+            <v-col cols="auto">
+              <v-btn variant="outlined" @click="homeClick">HOME</v-btn>
+            </v-col>
+            <v-col cols="auto">
+              <v-btn variant="outlined" @click="treeComponent2Click"
+                >TreeComponent2</v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-app-bar>
+
+
+        <!-- 連絡事項用のバナー -->
+        <v-banner color="blue" dark v-if="port_message_visible">
+          <template v-slot:action>
+            <v-btn text @click="dismissBanner">閉じる</v-btn>
+          </template>
+          ※検証環境DjangoのPortは8001です
+        </v-banner>
+        <v-banner color="blue" dark v-if="port_message_visible">
+          <template v-slot:action>
+            <v-btn text @click="dismissBanner">閉じる</v-btn>
+          </template>
+          <a href="http://133.18.242.137/api/test" blank="_self">133.18.242.137</a>
+        </v-banner>
+
+        <!--v-container-->
+        <v-container>
+          <router-view />
+        </v-container>
+      
+      
       </v-main>
     </v-app>
-
   </div>
 </template>
 <style scoped></style>
