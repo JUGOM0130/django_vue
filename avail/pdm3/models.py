cd6202ba97a26747d45969726b830df2730922d2
J.Tsuchiya <tsuchi-jun@asuzacgroup.jp>
2025-03-10 17:07:46 +0900

    model修正

--- avail/pdm3/models.py
+++ avail/pdm3/models.py
@@ -1,7 +1,60 @@
 from django.db import models, transaction
 from django.utils import timezone
 
+class Prefix(models.Model):
+    """コード生成のためのプレフィックスモデル"""
+    CODE_TYPE_CHOICES = [
+        ('1', '組'),
+        ('2', '部品'),
+        ('3', '購入品'),
+    ]
+    name = models.CharField(max_length=10, unique=True)  # プレフィックス名
+    description = models.TextField(blank=True, null=True)  # プレフィックスの説明
+    code_type = models.CharField(max_length=6, choices=CODE_TYPE_CHOICES)  # コードのタイプ
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")  # レコードの作成日時
+    update_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")  # レコードの更新日時
 
+    def __str__(self):
+        return self.name
+
+
+class Node(models.Model):
+    """個々のノードを表すモデル。ツリー内のすべてのノードが格納される"""
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
+
+    def __str__(self):
+        return self.name
+    
+class CodeCounter(models.Model):
+    """プレフィックスごとのカウンターと版数を管理するモデル"""
+    prefix = models.ForeignKey(Prefix, on_delete=models.CASCADE, related_name="counters")
+    current_number = models.PositiveIntegerField(default=0)  # 現在の採番値
+    current_version = models.PositiveIntegerField(default=0)  # 現在の版数
+
+    def __str__(self):
+        return f"{self.prefix.name}: {self.current_number}, Z{self.current_version}"
+
+class CodeVersion(models.Model):
+    """各コードごとに履歴管理されるテーブル"""
+    code = models.CharField(max_length=50, unique=True)  # コード
+    version = models.IntegerField(default=0)  # バージョン番号
+
+    def __str__(self):
+        return f"{self.code}, Z{self.version:04d}"
+
+class CodeVersionHistory(models.Model):
+    """コードのバージョン履歴を管理するテーブル"""
+    node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='version_histories')  # ノードとの関連
+    code = models.CharField(max_length=50)  # バージョン履歴に含まれるコード
+    version = models.IntegerField()  # バージョン番号
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")  # バージョンが作成された日時
+
+    def __str__(self):
+        return f"{self.node.name}: {self.code}, Z{self.version:04d} at {self.datetime_created}"
+    
 
 class Tree(models.Model):
     """ツリー自体を表すモデル。使い回される"""
@@ -97,57 +150,6 @@ class TreeVersion(models.Model):
         return f"{self.tree.name} v{self.version_number}: {self.version_name}"
     
 
-class Prefix(models.Model):
-    """コード生成のためのプレフィックスモデル"""
-    CODE_TYPE_CHOICES = [
-        ('1', '組'),
-        ('2', '部品'),
-        ('3', '購入品'),
-    ]
-    name = models.CharField(max_length=10, unique=True)  # プレフィックス名
-    description = models.TextField(blank=True, null=True)  # プレフィックスの説明
-    code_type = models.CharField(max_length=6, choices=CODE_TYPE_CHOICES)  # コードのタイプ
-    create_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")  # レコードの作成日時
-    update_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")  # レコードの更新日時
-
-    def __str__(self):
-        return self.name
 
 
 
-class Node(models.Model):
-    """個々のノードを表すモデル。ツリー内のすべてのノードが格納される"""
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    create_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
-    update_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
-
-    def __str__(self):
-        return self.name
-    
-class CodeCounter(models.Model):
-    """プレフィックスごとのカウンターと版数を管理するモデル"""
-    prefix = models.ForeignKey(Prefix, on_delete=models.CASCADE, related_name="counters")
-    current_number = models.PositiveIntegerField(default=0)  # 現在の採番値
-    current_version = models.PositiveIntegerField(default=0)  # 現在の版数
-
-    def __str__(self):
-        return f"{self.prefix.name}: {self.current_number}, Z{self.current_version}"
-
-class CodeVersion(models.Model):
-    """各コードごとに履歴管理されるテーブル"""
-    code = models.CharField(max_length=50, unique=True)  # コード
-    version = models.IntegerField(default=0)  # バージョン番号
-
-    def __str__(self):
-        return f"{self.code}, Z{self.version:04d}"
-
-class CodeVersionHistory(models.Model):
-    """コードのバージョン履歴を管理するテーブル"""
-    node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='version_histories')  # ノードとの関連
-    code = models.CharField(max_length=50)  # バージョン履歴に含まれるコード
-    version = models.IntegerField()  # バージョン番号
-    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")  # バージョンが作成された日時
-
-    def __str__(self):
-        return f"{self.node.name}: {self.code}, Z{self.version:04d} at {self.datetime_created}"
