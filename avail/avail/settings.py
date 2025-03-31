a2ce962a070c3af9cde73ce55169fe82f3c6c480
J.Tsuchiya <tsuchi-jun@asuzacgroup.jp>
2025-03-31 16:40:37 +0900

    コメント修正

--- avail/avail/settings.py
+++ avail/avail/settings.py
@@ -135,9 +135,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# python manage.py collectstatic で収集する静的ファイルの保存先
 STATIC_ROOT = 'static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = []
